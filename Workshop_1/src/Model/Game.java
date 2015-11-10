60b2d77e50eb69e3460aaa6b531332e69f595332
johanrovala <jr.johan.ro@gmail.com>
2015-11-10 15:16:06 +0100

    GREEN: test #1 on Game now succeds

--- Workshop_1/src/Model/Game.java
+++ Workshop_1/src/Model/Game.java
@@ -1,10 +1,13 @@
 package Model;
 
+import java.util.InputMismatchException;
+
 /**
  * Created by johanrovala on 10/11/15.
  */
 public class Game {
 
-    public Game (String chosenWord){
+    public Game (String chosenWord)throws InputMismatchException{
+        throw new InputMismatchException();
     }
 }
