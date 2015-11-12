97f63a714e058b56e14895f213faf04168ac8a6c
johanrovala <jr.johan.ro@gmail.com>
2015-11-12 13:42:59 +0100

    RED: Test # 1 in ControllerTest failed

---
+++ Workshop_1/src/ControllerTest/ControllerTest.java
@@ -0,0 +1,18 @@
+package ControllerTest;
+
+import Controller.Controller;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by johanrovala on 12/11/15.
+ */
+public class ControllerTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test(){
+        new Controller(null, null);
+    }
+
+}
