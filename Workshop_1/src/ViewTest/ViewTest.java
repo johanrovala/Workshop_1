92955a45609cc22ca5c6daec95a37f1f76ae84a9
johanrovala <jr.johan.ro@gmail.com>
2015-11-13 15:38:30 +0100

    REFACTOR: changed method signature of test # 1 in ViewTest

--- Workshop_1/src/ViewTest/ViewTest.java
+++ Workshop_1/src/ViewTest/ViewTest.java
@@ -34,14 +34,10 @@ public class ViewTest {
 
 
     @Test
-    public void out(){
+    public void shouldDisplayTheCorrectWelcomeMessage(){
         View view = new View();
         view.displayWelcomeMessage();
         assertEquals("Welcome to my shitty version of Hangman. /n Press 'P' to start playing or 'Q' to get out", outContent.toString());
-       //View view = new View();
-       //view.displayWelcomeMessage();
-       // System.out.println("Welcome to my shitty version of Hangman. /n Press 'P' to start playing or 'Q' to get out");
-      //  assertEquals("Welcome to my shitty version of Hangman. /n Press 'P' to start playing or 'Q' to get out", outContent.toString());
     }
 
 
